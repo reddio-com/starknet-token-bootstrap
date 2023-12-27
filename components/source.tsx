09e47e7d89118de1cdb96e3968732a44e1d02f07
neilydhan <59085685+neilydhan@users.noreply.github.com>
2023-12-27 21:32:57 +0800

    update github link

--- components/source.tsx
+++ components/source.tsx
@@ -2,7 +2,7 @@ export default function Source() {
   return (
     <div className="absolute bottom-4 right-4 flex gap-8">
       <a
-        href="https://github.com/subvisual/starknet-demo-dapp"
+        href="https://github.com/reddio-com/starknet-token-bootstrap"
         target="_blank"
         className="underline"
       >
